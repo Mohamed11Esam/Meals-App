8607472598971d48a9e400f097283843f2d047ab
Mohamed Esam <111438267+Mohamed11Esam@users.noreply.github.com>
2025-02-08 22:46:32 +0200

    edit z index side branch

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -30,7 +30,7 @@ function SideBar() {
       </button>
       <aside
         id="default-sidebar"
-        className="fixed top-0 left-0  w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#F9FAFB]"
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[#F9FAFB]"
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 overflow-y-auto ">
